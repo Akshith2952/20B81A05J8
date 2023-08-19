0a49106dfa3ab22d24313386bcf2a062a536cede
Akshith2952 <123850962+Akshith2952@users.noreply.github.com>
2023-08-19 16:36:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,6 @@ const axios = require("axios");
 const app = express();
 
 app.listen(8008);
-
-// function bubbleSort(array) {
-//   const n = array.length;
-//   for (let i = 0; i < n - 1; i++) {
-//     for (let j = 0; j < n - i - 1; j++) {
-//       if (array[j] > array[j + 1]) {
-//         const temp = array[j];
-//         array[j] = array[j + 1];
-//         array[j + 1] = temp;
-//       }
-//     }
-//   }
-//   return arr;
-// }
 function insertionSort(arr) {
   const n = arr.length;
   for (let i = 1; i < n; i++) {
